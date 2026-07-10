7caa318c4f5dfb47770d65bfd8a9428f6d4e712f
akshara2004 <aksharaa716@gmail.com>
2026-07-10 11:27:44 +0530

    changes-added

--- src/Components/Delete.jsx
+++ src/Components/Delete.jsx
@@ -11,7 +11,7 @@ const Delete = () => {
             </h1>
         <div className="container">
             <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <div className="row">
+                <div className="row g-3">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <label htmlFor="" className="form-label">Admission Number:</label>
                         <input type="text" className="form-control" />
--- src/Components/ViewStudents.jsx
+++ src/Components/ViewStudents.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewStudents = () => {
+    const [data,changeData]= useState([
+    {
+    name: "Akshara",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73OboNq8YpolvhWur1kpvkaggtHmHEzhY7RPohICuOA&s=10",
+    admissionNo: 345,
+    course: "MCA"
+     },
+      {
+    name: "John",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73OboNq8YpolvhWur1kpvkaggtHmHEzhY7RPohICuOA&s=10",
+    admissionNo: 235,
+    course: "CS"
+     },
+     {
+    name: "Jack",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73OboNq8YpolvhWur1kpvkaggtHmHEzhY7RPohICuOA&s=10",
+    admissionNo: 456,
+    course: "EEE"
+     },
+       {
+    name: "pushpa",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS73OboNq8YpolvhWur1kpvkaggtHmHEzhY7RPohICuOA&s=10",
+    admissionNo: 230,
+    course: "MCA"
+     }
+
+
+       
+    ])
   return (
     <div>
         <NavBar/>
-        <div classNameName="container" style={{padding:10,margin:50}}>
+        <div className="container" style={{padding:10,margin:50}}>
             <h1 style={{ color: "green", marginBottom: 50,  }}>
             ViewStudent
             </h1>
@@ -15,43 +44,42 @@ const ViewStudents = () => {
                     <div className="row">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl 6">
 
-                            <table class="table">
-                            <thead>
-                                <tr>
-                                <th scope="col">SI NO</th>
-                                <th scope="col">NAME</th>
-                                <th scope="col">ADMISSION NO.</th>
-                                <th scope="col">CLASS</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <th scope="row">1</th>
-                                <td>Akshara</td>
-                                <td>345</td>
-                                <td>MCA</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">2</th>
-                                <td>Archana</td>
-                                <td>456</td>
-                                <td>CS</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">3</th>
-                                <td>John</td>
-                                <td>309</td>
-                                <td>IT</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">4</th>
-                                <td>sara</td>
-                                <td>329</td>
-                                <td>EEE</td>
-                                </tr>
+                              <table className="table table-bordered table-striped table-hover">
+              <thead className="table-dark">
+                <tr>
+                  <th>Photo</th>
+                  <th>Name</th>
+                  <th>Admission No</th>
+                  <th>Course</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
 
-                            </tbody>
-                            </table>
+              <tbody>
+                {data.map((value, index) => {
+                  return (
+                  <tr>
+                    <td>
+                      <img
+                        src={value.avatar}
+                        alt={value.name}
+                        width="50"
+                        height="50"
+                      />
+                    </td>
+                    <td>{value.name}</td>
+                    <td>{value.admissionNo}</td>
+                    <td>{value.course}</td>
+                    <td>
+                      <button className="btn btn-primary">
+                        View Details
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+              </tbody>
+            </table>
 
 
                         </div>
